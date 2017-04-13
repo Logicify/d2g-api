eaa3d1d8d33450df68929a26f8877b09df57df06
Serhei <matrix.has.you.13@gmail.com>
2017-04-13 23:52:15 +0300

    added basicAuth

---
+++ d2g-webapi/src/main/java/com/logicify/d2g/web/controllers/D2GAuthConfiguration.java
@@ -0,0 +1,22 @@
+package com.logicify.d2g.web.controllers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * Created by Serhei on 13.04.2017.
+ */
+@Configuration
+public class D2GAuthConfiguration extends GlobalAuthenticationConfigurerAdapter {
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("USER", "ADMIN", "READER", "WRITER")
+                .and()
+                .withUser("audit").password("audit").roles("USER", "ADMIN", "READER");
+    }
+}
--- d2g-webapi/src/main/java/com/logicify/d2g/web/controllers/SecurityController.java
+++ d2g-webapi/src/main/java/com/logicify/d2g/web/controllers/SecurityController.java
@@ -1,11 +1,43 @@
 package com.logicify.d2g.web.controllers;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 /**
  * Created by jadencorr on 06.03.17.
  */
 
 @RestController
+@EnableWebSecurity
 public class SecurityController {
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
+
+    @Configuration
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .antMatchers("/login", "/").permitAll()
+                    .anyRequest().authenticated().and()
+                    .httpBasic()
+                    .and()
+                    .csrf().disable()
+            ;
+
+        }
+    }
+
+
 }

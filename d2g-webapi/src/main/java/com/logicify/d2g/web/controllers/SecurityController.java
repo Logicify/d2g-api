bd7692fc10f0ecd695391b4a6f26857c7e5cec7a
Serhei <matrix.has.you.13@gmail.com>
2017-04-18 10:24:56 +0300

    added some changes in basicAuth

--- d2g-webapi/src/main/java/com/logicify/d2g/web/controllers/SecurityController.java
+++ d2g-webapi/src/main/java/com/logicify/d2g/web/controllers/SecurityController.java
@@ -1,10 +1,11 @@
 package com.logicify.d2g.web.controllers;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.bind.annotation.*;
+import com.logicify.d2g.dtos.domain.dtos.ServiceInformation;
+import com.logicify.d2g.dtos.domain.outgoingdtos.ResponseDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
@@ -13,31 +14,20 @@ import java.security.Principal;
  */
 
 @RestController
-@EnableWebSecurity
 public class SecurityController {
 
-    @RequestMapping(path = "/user/login", method = RequestMethod.GET) // TODO: change this method, my change only for deploying
+    @RequestMapping(path = "/restore-session", method = RequestMethod.GET)
     @ResponseBody
-    public Principal user(Principal user) {
-        return user;
-    }
-
-
-    @Configuration
-    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .authorizeRequests()
-                    .antMatchers("/login", "/").permitAll()
-                    .anyRequest().authenticated().and()
-                    .httpBasic()
-                    .and()
-                    .csrf().disable()
-            ;
-
+    public ResponseDto getAuthUserData(Principal user) {
+        //TODO catch D2GBaseException
+        try {
+            ResponseDto response = new ResponseDto();
+            response.setService(new ServiceInformation());
+            return response;
+        } catch (Exception e) {
+            ResponseDto response = new ResponseDto();
+            response.setService(new ServiceInformation(e));
+            return response;
         }
     }
-
-
 }
